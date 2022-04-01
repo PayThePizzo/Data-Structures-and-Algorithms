9b96eb1a0944846572f4e6346e8add335a0e341d
PayThePizzo <44942410+PayThePizzo@users.noreply.github.com>
2022-04-01 10:51:43 +0200

    Started graphs Fixing Ex1_Assignment1

--- Assignment/first_assignment/ex_1.cpp
+++ Assignment/first_assignment/ex_1.cpp
@@ -1,11 +1,5 @@
 // Exercise 1:
-/*
- * Pre-requisites:
- * - Balanced Tree
- * - Arch
- * - Terminable Arch
- * - 3-balanced Tree
- */
+
 #include "first_assignment.h"
 #include <algorithm>
 
@@ -13,7 +7,7 @@ using namespace std;
 
 //TODO: Test if balanced
 int treBil_aux_rec(PNode root){
-    if (root->left && root->right) {        // both children
+    if (root->left && root->right) {        //both children
         return std::max(treBil_aux_rec(root->left)+1,
                    treBil_aux_rec(root->right)+1);
     }
@@ -30,12 +24,14 @@ int treBil_aux_rec(PNode root){
 
 
 bool treBil(PNode u){
-    if(u != nullptr){
+    if(u != nullptr)
         return treBil_aux_rec(u)<=3 ? true : false;
-    }
+
     return true; // if it is an empty tree, it is true by definition.
 }
 
-//=======================================================================
-
-// Evaluation of the complexity
+/*
+*
+ * Control
+ *
+*/
