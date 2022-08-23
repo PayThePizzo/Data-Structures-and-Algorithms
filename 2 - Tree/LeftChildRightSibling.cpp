38fa6c0eaf95c1878ab787d1754c77734d7ad084
PayThePizzo <44942410+PayThePizzo@users.noreply.github.com>
2022-08-23 17:11:03 +0200

    Corrections

--- 2 - Tree/LeftChildRightSibling.cpp
+++ 2 - Tree/LeftChildRightSibling.cpp
@@ -3,3 +3,60 @@
 //
 
 #include "LeftChildRightSibling.h"
+#include <iostream>
+
+using namespace binlcrs;
+
+LeftChildRightSibling* newTree(int info){
+    //Allocation
+    LeftChildRightSibling *root = (LeftChildRightSibling*)malloc(sizeof (LeftChildRightSibling));
+    root->key=info;
+    root->p = nullptr;
+    root->left_c = nullptr;
+    root->right_s = nullptr;
+    return root;
+}
+
+bool treeEmpty(LeftChildRightSibling* tree){
+    return (tree == nullptr);
+}
+
+void insert(LeftChildRightSibling* tree, LeftChildRightSibling* node){
+    if(tree != nullptr && node != nullptr){
+        LeftChildRightSibling iter = tree->left_c;
+        while(iter != nullptr){
+            iter = iter->right_s;
+        }
+        iter = node;
+    }
+    return;
+}
+
+
+LeftChildRightSibling parent(LeftChildRightSibling tree, LeftChildRightSibling node){
+    return *Node.p;
+}
+
+void children(LeftChildRightSibling tree, LeftChildRightSibling node){
+
+}
+
+void depthfirstsearch(LeftChildRightSibling tree){
+
+}
+
+void symmetric_visit(LeftChildRightSibling tree){
+
+}
+
+void post_order_visit(LeftChildRightSibling tree){
+
+}
+
+void breadfirstsearch(LeftChildRightSibling tree){
+
+}
+
+void print_tree(LeftChildRightSibling* root){
+
+}
--- 2 - Tree/LeftChildRightSibling.h
+++ 2 - Tree/LeftChildRightSibling.h
@@ -4,11 +4,79 @@
 
 #ifndef ASD_LEFTCHILDRIGHTSIBLING_H
 #define ASD_LEFTCHILDRIGHTSIBLING_H
+#include <iostream>
+
+namespace binaryleftchildrightsiblingtree{
+
+    typedef struct Tree{
+        int key;
+        struct Tree *left_c;
+        struct Tree *right_s;
+        struct Tree *p;
+    } Tree;
+
+    /**
+     * Returns a binarized LeftChildRightSibling new tree
+     *
+     * @param A
+     * @return
+     */
+    Tree* newTree(int info);
+
+    bool treeEmpty(Tree* tree);
+
+    void insert(Tree* tree, Tree* node);
+
+    void parent(Tree tree, Tree Node);
+
+    void children(Tree tree, Tree node);
+
+    void depthfirstsearch(Tree tree);
+
+    void symmetric_visit(Tree tree);
+
+    void post_order_visit(Tree tree);
+
+    void breadfirstsearch(Tree tree);
+
+    void print_tree(Tree* root);
+
+}
+
+namespace genericbinarytree{
+
+    typedef struct Tree{
+        int key;
+        struct LeftChildRightSibling *left;
+        struct LeftChildRightSibling *right;
+        struct LeftChildRightSibling *p;
+    } Tree;
+
+    Tree* newTree(int info);
+
+    void insert(Tree* tree, Tree* node);
+
+    void parent(Tree* tree, Tree* Node);
+
+    void children(Tree* tree, Tree* node);
+
+    void depthfirstsearch(Tree* tree);
+
+    void symmetric_visit(Tree* tree);
+
+    void post_order_visit(Tree* tree);
+
+    void breadfirstsearch(Tree* tree);
+
+    void print_tree(Tree* root);
+}
+
+namespace binarysearchtree{
 
 
-class LeftChildRightSibling {
+}
 
-};
+int main(){
 
-
+}
 #endif //ASD_LEFTCHILDRIGHTSIBLING_H
