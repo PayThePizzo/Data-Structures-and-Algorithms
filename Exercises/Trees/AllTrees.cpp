92c450c3ff63d9221c4f4d66dbb1c4111eb4b053
PayThePizzo <44942410+PayThePizzo@users.noreply.github.com>
2022-08-26 13:09:57 +0200

    Exercises

---
+++ Exercises/Trees/AllTrees.cpp
@@ -0,0 +1,35 @@
+//
+// Created by PayThePizzo on 8/26/2022.
+//
+
+namespace tree{
+
+    void new_tree(){
+
+    }
+
+    void tree_empty(){
+
+    }
+
+    void father(){
+
+    }
+
+    void children(){
+
+    }
+
+}
+
+namespace binarytree{
+
+
+}
+
+namespace binarysearchtree{
+
+
+}
+
+namespace lefg
