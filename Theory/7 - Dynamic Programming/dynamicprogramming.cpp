be7be4ff95efcbc92bb57da1d8d741e791c22ba6
PayThePizzo <44942410+PayThePizzo@users.noreply.github.com>
2023-03-31 14:01:46 +0200

    Dynamic Programming

--- Theory/7 - Dynamic Programming/dynamicprogramming.cpp
+++ Theory/7 - Dynamic Programming/dynamicprogramming.cpp
@@ -7,23 +7,106 @@
 
 using namespace std;
 
-int Naive_Cut_Rod(vector<int> p, int n) {
+// Cut Rod Solutions
+
+int Naive_CR(vector<int> p, int n) {
     if (n == 0) {
         return 0;
     }else{
         int q = -1;
-        for(int i = 0; i<n; i++) {
-            q = max(q, p[i] + Naive_Cut_Rod(p, n - i));
+        for(int i = 1; i<=n; i++) {
+            q = max(q, p[i] + Naive_CR(p, n - i));
         }
         return q;
     }
 }
 
+int Memoized_CR_Aux(vector<int> p, vector<int> r, int j){
+    if(r[j]<0){
+        if(j==0){
+            r[j]=0;
+        }else{
+            int q = -1;
+            for(int i = 1; i<=j; i++){
+                q = max(q, p[i]+ Memoized_CR_Aux(p, r, j-i));
+            }
+            r[j]=q;
+        }
+    }
+    return r[j];
+}
+
+int Memoized_CR(vector<int> p, int n){
+    vector<int> r;
+    for(int i = 0; i<=n; i++){
+        r.push_back(-1);
+    }
+    return Memoized_CR_Aux(p, r, n);
+}
+
+int BU_CR(vector<int> p, int n){
+    vector<int> r;
+    for(int k=0; k<=n; k++){
+        r.push_back(-1);
+    }
+    r[0]=0;
+    for(int j=1; j<=n;j++){
+        int q=-1;
+        for(int i=1; i<=j; i++){
+            q = max(q, p[i]+r[j-i]);
+        }
+        r[j]=q;
+    }
+    return r[n];
+}
+
+std::pair<vector<int>, vector<int>> BU_CR2(vector<int> p, int n){
+    vector<int> r;
+    vector<int> s;
+    for(int k=0; k<=n; k++){
+        r.push_back(-1);
+        s.push_back(-1);
+    }
+    r[0]=0;
+    s[0]=0;
+    for(int j=1; j<=n;j++){
+        int q=-1;
+        for(int i=1; i<=j; i++){
+            if(q < p[i]+r[j-i]){
+                q = p[i]+r[j-i];
+                s[j]=i;
+            }
+        }
+        r[j]=q;
+    }
+    std::pair<vector<int>, vector<int>> sol = {r,s};
+    return sol;
+}
+
+void Print_CR_BU_CR2(vector<int>p, int n){
+    std::pair<vector<int>, vector<int>> sol = BU_CR2(p, n);
+    int tot = 0;
+    while(n>0){
+        tot+= sol.first[sol.second[n]];
+        n-= sol.second[n];
+    }
+    cout << "" << tot;
+}
+
+// LCS Solutions
+
+
+
 int main()
 {
-    vector<int> p = {1,3,9,6,7,10, 15};
-    int q = Naive_Cut_Rod(p, 6);
-    cout << "" << q;
+    vector<int> p = {0, 1,112,9,6,7,10, 18, 42, 55, 10};
+    int n = 3;
+
+    int max = BU_CR(p, n);
+    cout << "" << max << "\n";
+
+    Print_CR_BU_CR2(p, 3);
+
 
     return 0;
 }
